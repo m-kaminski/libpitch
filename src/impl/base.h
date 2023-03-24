aa5f433817f5986c3896a7bf5726bdaf04916e98
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-24 14:57:45 -0500

    Renamed base36 to generic base

--- src/impl/base.h
+++ src/impl/base.h
@@ -1,24 +1,13 @@
-#ifndef _BASE36_H
-#define _BASE36_H
+#ifndef _LIBPITCH_IMPL_TYPES_BASE36_H
+#define _LIBPITCH_IMPL_TYPES_BASE36_H
 #include <cstdint>
 #include <stdexcept>
 namespace pitch::types {
-    static const std::uint64_t base36_max = 4738381338321616895LL;
 
-    /***
-     * Convert a range of characters into base36 number. Accepts 0-9 and A-Z as digits
-     * type is a forward iterator to a char
-     * @param begin iterator to a begining of number to convert
-     * @param end iterator to an ending of number to convert
-     * @throw std::invalid_argument if more than 12 base36 digits or incorrect digit
-     */
-    template <typename T>
-    std::uint64_t base36(T begin, const T &end)
+
+    template <int base, typename T>
+    std::uint64_t read_base(T begin, const T &end)
     {
-        if (end - begin > 12)
-        {
-            throw std::invalid_argument("Input string too long (12 digits at most)");
-        }
         std::uint64_t result = 0;
         while (begin != end)
         {
@@ -29,7 +18,7 @@ namespace pitch::types {
                 d = *begin - 'A' + 10;
             else
                 throw std::invalid_argument("Not a base36 digit");
-            result *= 36;
+            result *= base;
             result += d;
 
             begin++;
@@ -39,14 +28,26 @@ namespace pitch::types {
     };
 
     /***
+     * Convert a range of characters into base36 number. Accepts 0-9 and A-Z as digits
+     * type is a forward iterator to a char
+     * @param begin iterator to a begining of number to convert
+     * @param end iterator to an ending of number to convert
+     * @throw std::invalid_argument if more than 12 base36 digits or incorrect digit
+     */
+    template <typename T>
+    std::uint64_t read_base36(T begin, const T &end) {
+        return read_base<36>(begin, end);
+    }
+
+    /***
      * Convert a string-equivalent to base36 number
      * @param s string-equivalent input, at most 12-characters long
      * @throw std::invalid_argument if more than 12 base36 digits or incorrect digit
      */
     template <typename T>
-    std::uint64_t base36(T s)
+    std::uint64_t read_base36(T s)
     {
-        return base36(s.begin(), s.end());
+        return read_base36(s.begin(), s.end());
     }
 
 }
--- src/impl/base_test.cpp
+++ src/impl/base_test.cpp
@@ -1,14 +1,17 @@
 #include "gtest/gtest.h"
-
-#include "base36.h"
 #include <iostream>
 #include <string>
 #include <cassert>
+
+#include "base.h"
+
 
 namespace pitch
 {
     namespace types
     {
+        static const std::uint64_t base36_max = 4738381338321616895LL; //  value of 12 'Z' characters (largest all-Z sequence)
+
         class base36_test : public ::testing::Test
         {
         };
@@ -20,11 +23,11 @@ namespace pitch
             std::string B36_144("40");
             std::string B36_948437811("FOOBAR");
             std::string B36_MAX("ZZZZZZZZZZZZ");
-            EXPECT_EQ(base36(B36_1), 1);
-            EXPECT_EQ(base36(B36_10), 10);
-            EXPECT_EQ(base36(B36_144), 144);
-            EXPECT_EQ(base36(B36_948437811), 948437811);
-            EXPECT_EQ(base36(B36_MAX), base36_max);
+            EXPECT_EQ(read_base36(B36_1), 1);
+            EXPECT_EQ(read_base36(B36_10), 10);
+            EXPECT_EQ(read_base36(B36_144), 144);
+            EXPECT_EQ(read_base36(B36_948437811), 948437811);
+            EXPECT_EQ(read_base36(B36_MAX), base36_max);
             EXPECT_EQ(B36_MAX.size(), 12);
         }
 
@@ -34,45 +37,31 @@ namespace pitch
             std::string B36_144("40");
             std::string B36_948437811("FOOBAR");
             std::string B36_MAX("ZZZZZZZZZZZZ");
-            EXPECT_EQ(base36(B36_10.begin(), B36_10.end()), 10);
-            EXPECT_EQ(base36(B36_144.begin(), B36_144.end()), 144);
-            EXPECT_EQ(base36(B36_948437811.begin(), B36_948437811.end()), 948437811);
-            EXPECT_EQ(base36(B36_MAX.begin(), B36_MAX.end()), base36_max);
+            EXPECT_EQ(read_base36(B36_10.begin(), B36_10.end()), 10);
+            EXPECT_EQ(read_base36(B36_144.begin(), B36_144.end()), 144);
+            EXPECT_EQ(read_base36(B36_948437811.begin(), B36_948437811.end()), 948437811);
+            EXPECT_EQ(read_base36(B36_MAX.begin(), B36_MAX.end()), base36_max);
         }
 
         TEST_F(base36_test, conversion_0)
         {
             std::string B36_0("0");
-            EXPECT_EQ(base36(B36_0.begin(), B36_0.end()), 0);
+            EXPECT_EQ(read_base36(B36_0.begin(), B36_0.end()), 0);
         }
 
         TEST_F(base36_test, conversion_empty)
         {
             std::string B36_E(""); // empty string converted to 0
-            EXPECT_EQ(base36(B36_E.begin(), B36_E.end()), 0);
-        }
-
-        TEST_F(base36_test, conversion_too_long_throw)
-        {
-            std::string B36_L("1234567890123"); // 13 character
-            EXPECT_THROW(base36(B36_L.begin(), B36_L.end()), std::invalid_argument);
-            try
-            {
-                base36(B36_L.begin(), B36_L.end());
-            }
-            catch (const std::invalid_argument &e)
-            {
-                EXPECT_STREQ("Input string too long (12 digits at most)", e.what());
-            }
+            EXPECT_EQ(read_base36(B36_E.begin(), B36_E.end()), 0);
         }
 
         TEST_F(base36_test, conversion_lowercase_throw)
         {
             std::string B36_L("foobar"); // Lowercase
-            EXPECT_THROW(base36(B36_L.begin(), B36_L.end()), std::invalid_argument);
+            EXPECT_THROW(read_base36(B36_L.begin(), B36_L.end()), std::invalid_argument);
             try
             {
-                base36(B36_L.begin(), B36_L.end());
+                read_base36(B36_L.begin(), B36_L.end());
             }
             catch (const std::invalid_argument &e)
             {
