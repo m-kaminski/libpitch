95882b83a0ff51c1e488b38c29f4e37158f0c189
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-24 17:11:47 -0500

    Expanded testing

--- src/impl/padded_string_test.cpp
+++ src/impl/padded_string_test.cpp
@@ -28,7 +28,7 @@ namespace pitch
             EXPECT_STREQ("QQQ", to.c_str());
         }
 
-        TEST_F(padded_string_test, string_extract_middle)
+        TEST_F(padded_string_test, string_extract_after)
         {
             std::string from("  QQQ    ");
             std::string to("X");
@@ -36,11 +36,33 @@ namespace pitch
             EXPECT_STREQ("QQQX", to.c_str());
         }
 
+        TEST_F(padded_string_test, string_extract_middle)
+        {
+            std::string from("  QQQ    ");
+            std::string to("XY");
+            copy_padded_string(from.begin()+2, from.end(), inserter(to, to.begin()+1));
+            EXPECT_STREQ("XQQQY", to.c_str());
+        }
+
         TEST_F(padded_string_test, string_extract_get_string)
         {
             std::string from("QQQ    ");
             std::string to(get_padded_string(from.begin(), from.end()));
             EXPECT_STREQ("QQQ", to.c_str());
+        }
+
+        TEST_F(padded_string_test, string_invalid)
+        {
+            std::string from("QQQ   A ");
+            EXPECT_THROW(get_padded_string(from.begin(), from.end()), std::invalid_argument);
+            try
+            {
+                get_padded_string(from.begin(), from.end());
+            }
+            catch (const std::invalid_argument &e)
+            {
+                EXPECT_STREQ("Invalid space padded string", e.what());
+            }
         }
     }
 }
