c4ea049caa252fba7409b1b042b9cace8c2f12ec
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-24 16:12:49 -0500

    Added tests covering bases 35, 16 and 2, just for sake of completeness

--- src/impl/base_test.cpp
+++ src/impl/base_test.cpp
@@ -43,6 +43,46 @@ namespace pitch
             EXPECT_EQ(read_base<10>(B10_MAX.begin(), B10_MAX.end()), 9223372036854775807);
         }
 
+
+        TEST_F(base_test, conversion_base_35)
+        {
+            std::string B35_1("1");
+            std::string B35_10("A");
+            std::string B35_155("4F");
+            std::string B35_824885977("FOOBAR");
+            std::string B35_MAX("ZZZZZZZZZZZZ");
+            EXPECT_EQ(read_base<35>(B35_1), 1);
+            EXPECT_EQ(read_base<35>(B35_10), 10);
+            EXPECT_EQ(read_base<35>(B35_155), 155);
+            EXPECT_EQ(read_base<35>(B35_824885977), 824885977);
+            EXPECT_EQ(read_base<35>(B35_MAX), 3478609346528894760);
+            EXPECT_EQ(B35_MAX.size(), 12);
+        }
+
+        TEST_F(base_test, conversion_base_16)
+        {
+            std::string B35_1("1");
+            std::string B35_10("A");
+            std::string B35_155("9B");
+            std::string B35_824885977("312AC2D9");
+            EXPECT_EQ(read_base<16>(B35_1), 1);
+            EXPECT_EQ(read_base<16>(B35_10), 10);
+            EXPECT_EQ(read_base<16>(B35_155), 155);
+            EXPECT_EQ(read_base<16>(B35_824885977), 824885977);
+        }
+
+        TEST_F(base_test, conversion_base_2)
+        {
+            std::string B35_1("1");
+            std::string B35_10("1010");
+            std::string B35_155("10011011");
+            std::string B35_824885977("110001001010101100001011011001");
+            EXPECT_EQ(read_base<2>(B35_1), 1);
+            EXPECT_EQ(read_base<2>(B35_10), 10);
+            EXPECT_EQ(read_base<2>(B35_155), 155);
+            EXPECT_EQ(read_base<2>(B35_824885977), 824885977);
+        }
+
     }
 }
 
