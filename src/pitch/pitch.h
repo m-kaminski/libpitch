25992b70165086630607a32deb88f4cfc25097b2
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-25 14:06:36 -0500

    Add a switch statement to differentiate 'E' vs 'A'

--- src/pitch/pitch.h
+++ src/pitch/pitch.h
@@ -1,6 +1,8 @@
 #ifndef _LIBPITCH_PITCH_H
 #define _LIBPITCH_PITCH_H
 #include <memory>
+#include <array>
+#include <functional>
 #include "messages/message.h"
 namespace pitch
 {
@@ -15,20 +17,47 @@ namespace pitch
     class decoder
     {
     public:
+
+    
         /***
          * @param T2 input type of an iterator
          * @param begin begining of line to decode
          * @param end end of line to decode
          */
         template <typename T2>
-        T1 decode_message(T2 begin, T2 end)
+        static T1 decode_message(T2 begin, T2 end)
         {
             T1 p_result;
+            if (end - begin < 9)
+                return p_result;
+            // message type is always single character at offset 8
+            switch (*(begin+8)) {
+                case 'E': return decode_message_order_executed(begin, end);
+                case 'A': return decode_message_add_order(begin, end);
+                default: ;
+            }
+            return p_result;
+        }
 
-            p_result.reset(new pitch::messages::message);
+
+        template <typename T2>
+        static T1 decode_message_order_executed(T2 begin, T2 end) {
+             T1 p_result;
+
+            p_result.reset(new pitch::messages::order_executed);
 
             return p_result;
         }
+
+        template <typename T2>
+        static T1 decode_message_add_order(T2 begin, T2 end) {
+             T1 p_result;
+
+            p_result.reset(new pitch::messages::add_order);
+
+            return p_result;  
+        }
+
     };
 }
 
--- src/pitch/pitch_test.cpp
+++ src/pitch/pitch_test.cpp
@@ -15,33 +15,47 @@ namespace pitch
 
     TEST_F(pitch_test, construct_shared)
     {
-        std::string line;
+        std::string line("29000600EA000ABDDCF0X0000200000ZAB00091");
         decoder<std::shared_ptr<messages::message>> d;
         auto s_p =
             d.decode_message(line.begin(), line.end());
-        EXPECT_EQ(s_p->get_type(), messages::message::message_type::unknown_message);
+        EXPECT_EQ(s_p->get_type(), messages::message::message_type::order_executed);
         static_assert(std::is_same<decltype(s_p), std::shared_ptr<messages::message>>::value,
                       "expecting a shared pointer");
     }
 
     TEST_F(pitch_test, construct_unique)
     {
-        std::string line;
+        std::string line("29000600EA000ABDDCF0X0000200000ZAB00091");
         decoder<std::unique_ptr<messages::message>> d;
         auto u_p = d.decode_message(line.begin(), line.end());
-        EXPECT_EQ(u_p->get_type(), messages::message::message_type::unknown_message);
+        EXPECT_EQ(u_p->get_type(), messages::message::message_type::order_executed);
         static_assert(std::is_same<decltype(u_p), std::unique_ptr<messages::message>>::value,
                       "expecting a unique pointer");
     }
 
-    TEST_F(pitch_test, construct_default)
+    TEST_F(pitch_test, construct_default_executed)
     {
-        std::string line;
+        std::string line("29000600EA000ABDDCF0X0000200000ZAB00091");
         decoder d;
         auto u_p = d.decode_message(line.begin(), line.end());
-        EXPECT_EQ(u_p->get_type(), messages::message::message_type::unknown_message);
+        EXPECT_EQ(u_p->get_type(), messages::message::message_type::order_executed);
         static_assert(std::is_same<decltype(u_p), std::unique_ptr<messages::message>>::value,
                       "expecting a unique pointer");
+        EXPECT_NO_THROW(dynamic_cast<messages::order_executed&>(*u_p));
+        EXPECT_NE(nullptr, dynamic_cast<messages::order_executed*>(u_p.get()));
+    }
+
+    TEST_F(pitch_test, construct_default_add)
+    {
+        std::string line("29000020AA000ABDDCF0XS000300AMD   0000213700Y");
+        decoder d;
+        auto u_p = d.decode_message(line.begin(), line.end());
+        EXPECT_EQ(u_p->get_type(), messages::message::message_type::add_order);
+        static_assert(std::is_same<decltype(u_p), std::unique_ptr<messages::message>>::value,
+                      "expecting a unique pointer");
+        EXPECT_NO_THROW(dynamic_cast<messages::add_order&>(*u_p));
+        EXPECT_NE(nullptr, dynamic_cast<messages::add_order*>(u_p.get()));
     }
 }
 
